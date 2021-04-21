c2ede3d2a885672029e9c99da05a0b239a34df5f
Laban Bore <33986848+borelaban@users.noreply.github.com>
2021-04-21 07:10:06 +0300

    Delete CNN.R

--- CNN.R
+++
@@ -1,5 +0,0 @@
-library(tensorflow)
-library(keras)
-cifar <- dataset_cifar10()
-class_names <- c('airplane', 'automobile', 'bird', 'cat', 'deer',
-                 'dog', 'frog', 'horse', 'ship', 'truck')
